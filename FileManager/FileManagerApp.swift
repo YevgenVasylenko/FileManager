6f7dab164a9595f8910d57a9d94143eeb91902a4
Yevgen Vasylenko <yevgenvasylenko93@gmail.com>
2023-06-22 13:32:16 +0300

    Initial commit

---
+++ FileManager/FileManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManagerApp.swift
+//  FileManager
+//
+//  Created by Yevgen Vasylenko on 22.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FileManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
